acaba25e1aa0effc8935ec987125fcaf3075d20e
3DESIGN ROOTS <rraveendran983@gmail.com>
2025-08-26 15:42:15 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,323 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-reception",
+      "name": "RECEPTION",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.04652210409205715,
+        "pitch": -0.00040703541545994426,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.18519293274231785,
+          "pitch": 0.47138838873516065,
+          "rotation": 17.27875959474387,
+          "target": "0-reception"
+        },
+        {
+          "yaw": -1.1572222738829687,
+          "pitch": 0.41355169461210295,
+          "rotation": 0,
+          "target": "2-md-cabin"
+        },
+        {
+          "yaw": -1.4171625128499006,
+          "pitch": 0.19393797810228364,
+          "rotation": 0.7853981633974483,
+          "target": "4-accounts-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-conference-room",
+      "name": "CONFERENCE ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 3.121404726307368,
+        "pitch": 0.1441910361683103,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4212453241373675,
+          "pitch": 0.5434359534230175,
+          "rotation": 4.71238898038469,
+          "target": "4-accounts-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-md-cabin",
+      "name": "MD CABIN",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.0505481672239636,
+        "pitch": -0.032874898827596866,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.243592687878893,
+          "pitch": -0.023654006422788143,
+          "rotation": 0,
+          "target": "1-conference-room"
+        },
+        {
+          "yaw": -1.9764644187050369,
+          "pitch": 0.40073339273034847,
+          "rotation": 0,
+          "target": "1-conference-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-editors-room",
+      "name": "EDITORS ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -1.5855646567620827,
+        "pitch": 0.009276336369488547,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.059140696746855,
+          "pitch": 0.5631657271527502,
+          "rotation": 0,
+          "target": "5-dining"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-accounts-room",
+      "name": "ACCOUNTS ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.5720082478162052,
+        "pitch": 0.019993269609679842,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7722285382772558,
+          "pitch": 0.596153746044024,
+          "rotation": 0,
+          "target": "3-editors-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-dining",
+      "name": "DINING",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.5851069666365483,
+        "pitch": 0.02842371713975389,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.357666380523181,
+          "pitch": 0.5554686430637457,
+          "rotation": 4.71238898038469,
+          "target": "6-wash-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-wash-area",
+      "name": "WASH AREA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.09532182837815739,
+        "pitch": 0.02597374183462442,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4515387811551506,
+          "pitch": 0.12128905861813166,
+          "rotation": 0,
+          "target": "0-reception"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
